396a06900aa32b0cb1c87a5e3142bad670b1d509
Manuel Simoni <msimoni@gmail.com>
2022-08-20 18:06:05 +0200

    repl-basic

---
+++ tool/repl/repl.mjs
@@ -0,0 +1,106 @@
+import { VM } from "../../dist/lispx-vm.min.mjs";
+import { init_repl_stream } from "./repl_stream.mjs";
+
+const REPL_CODE = `
+;; This code expects that 'repl:+input-buffer+' as well as related
+;; functions are provided by JS.  See the file 'repl_stream.js'.
+;;
+;; 'repl:%%exit' which must also be provided is called to quit the REPL.
+
+(defconstant repl:+root-prompt+ 'repl:root-prompt
+  "The prompt used for blocking on IO.")
+
+(defconstant repl:+environment+ (the-environment)
+  "The environment in which REPL expressions are evaluated.")
+
+(defun repl:run ()
+  "Run the REPL."
+  (push-prompt repl:+root-prompt+
+    (restart-case ((repl:quit (lambda () (repl:%%exit))))
+      (loop
+        (fresh-line)
+        (repl:%%display-waiting-for-input-sign repl:+input-buffer+)
+        (print (eval (read) repl:+environment+))))))
+
+(defmethod stream-read ((stream repl:input-buffer) . #ignore)
+  "Blocking input function for the REPL input buffer.  This gets
+called from 'read' when the input stream is a 'repl:input-buffer'.
+
+Calls the built-in default implementation of 'read', which is
+non-blocking, i.e. immediately signals 'end-of-file' instead of
+waiting for more input.
+
+Tries the built-in 'read' on the current contents of the input buffer.
+If it successfully reads an object, return it.  If it signals
+'end-of-file', save the current continuation in the input buffer's
+wake-up function.  The wake-up function will be called when more input
+is available, in which case we start over.
+
+Note that the method ignores the generic function's 'eof-error-p' and
+'eof-value' parameters because it always blocks on EOF.
+
+To sum up, this method turns 'read' from non-blocking into blocking
+when the input stream is a 'repl:input-buffer'."
+  (block exit
+    (loop
+      ;; Use a trampoline to avoid stack build-up.
+      ((block trampoline
+         ;; Get a stream containing the current contents of the input buffer.
+         (let ((stream (repl:%%make-input-buffer-stream repl:+input-buffer+)))
+           (handler-case ((end-of-file
+                           (lambda #ignore
+                             ;; We got an end-of-file error: jump into trampoline
+                             ;; and save continuation in wake-up function.
+                             (return-from trampoline
+                                          (lambda ()
+                                            (take-subcont repl:+root-prompt+ k
+                                              (repl:%%set-input-buffer-wake-up-function
+                                               repl:+input-buffer+
+                                               (lambda ()
+                                                 (push-delim-subcont
+                                                  repl:+root-prompt+ k)))))))))
+               ;; Call built-in, non-blocking 'read' on the stream.
+               ;; This calls the built-in because the stream is a
+               ;; 'string-input-stream'.
+               (let ((form (read stream)))
+                 ;; We've successfully read an object: remove the
+                 ;; input we've consumed from the input buffer.
+                 (repl:%%truncate-input-buffer repl:+input-buffer+ stream)
+                 (return-from exit form)))))))))
+
+(defun repl:quit ()
+  "Quits the REPL."
+  (invoke-restart 'repl:quit))
+`;
+
+$(function() {
+
+    const vm = new VM();
+    init_repl_stream(vm);
+    vm.eval_js_string(REPL_CODE);
+
+    const term = $('#terminal').terminal(command_interpreter, {
+        greetings: vm.write_to_js_string(vm.eval_js_string(`"Welcome to Nybble Lisp!"`))
+    });
+
+    vm.STANDARD_OUTPUT.set_value(new vm.REPL_output_stream(function (output) {
+        term.echo(output.to_js_string(), { newline: false });
+    }));
+
+    const input_buffer = new vm.REPL_input_buffer();
+    vm.STANDARD_INPUT.set_value(input_buffer);
+
+    function command_interpreter(line)
+    {
+        input_buffer.add_line(vm.str(line + "\n"));
+        /*
+         * Force all output after each evaluation.
+         */
+        vm.STANDARD_OUTPUT.get_value().force_output();
+    }
+
+    vm.define_constant("repl:+input-buffer+", input_buffer);
+    vm.define_alien_function("repl:%%exit", () => exit(0));
+    vm.eval_js_string("(repl:run)");
+
+});
---
+++ tool/repl/repl_stream.mjs
@@ -0,0 +1,193 @@
+/*
+ * REPL Input and Output Streams
+ * Copyright (c) 2021 Manuel Simoni
+ */
+
+/*
+ * Adds REPL streams to the VM.
+ */
+export function init_repl_stream(vm)
+{
+    /*
+     * Output stream that buffers incoming bytes and sends them to an
+     * output function when forced or when a newline is printed.
+     */
+    vm.REPL_output_stream = class REPL_output_stream extends vm.Output_stream
+    {
+        /*
+         * Constructs a new REPL output stream that outputs to the
+         * given function.
+         */
+        constructor(output_function)
+        {
+            super();
+            this.output_function = vm.assert_type(output_function, "function");
+            this.buffer = "";
+            /*
+             * This variable keeps track of whether we are at the beginning
+             * of a fresh line for purposes of fresh_line().
+             */
+            this.line_is_fresh = true;
+        }
+
+        /*
+         * Accumulate a byte.  When a newline is written, force the
+         * current buffer contents to the output function.
+         */
+        write_byte(b)
+        {
+            vm.assert_type(b, "string");
+            vm.assert(b.length === 1);
+            this.buffer += b;
+            if (b === "\n") {
+                this.line_is_fresh = true;
+                this.force_output();
+            } else {
+                this.line_is_fresh = false;
+            }
+            return b;
+        }
+
+        /*
+         * If the buffer is non-empty, send it to the output function.
+         */
+        force_output()
+        {
+            if (this.buffer !== "") {
+                const buffer = this.buffer;
+                this.buffer = "";
+                this.output_function(new vm.String(buffer));
+            }
+            return vm.void();
+        }
+
+        /*
+         * Output stream standard method to print a newline if we
+         * aren't already on a fresh line.
+         */
+        fresh_line()
+        {
+            if (!this.line_is_fresh) {
+                this.write_byte("\n");
+                return vm.t();
+            } else {
+                return vm.f();
+            }
+        }
+    };
+
+    /*
+     * A REPL input buffer asynchronously receives lines from the user,
+     * for example via Node's readline module.  On the Lisp side,
+     * we want to have a blocking, synchronous READ function.
+     *
+     * Every time new data arrives, a wake-up function set by Lisp is
+     * called.  It contains a saved continuation.
+     *
+     * Lisp gets a fresh input stream to read the current contents
+     * of the input buffer.
+     *
+     * Once an object has been successfully read, the input buffer
+     * is truncated by the amount of data read.
+     *
+     * If an object can't be read because there isn't enough data in
+     * the buffer yet, Lisp saves the new continuation in the wake-up
+     * function again, and we start over.
+     *
+     * Note that we set an instance of this input buffer class as
+     * standard input on the Lisp side, even though it doesn't even
+     * implement vm.Input_stream!  But it's no problem, because we
+     * don't expose methods like READ-BYTE to Lisp yet, just READ.
+     * And READ internally calls the READ-STREAM method, which we
+     * specialize for the input buffer class.
+     */
+    vm.REPL_input_buffer = class REPL_input_buffer extends vm.Object
+    {
+        /*
+         * Construct a new input buffer.
+         *
+         * Lisp will call the provided display input function when it
+         * is ready to read a form.  It should display a prompt
+         * symbol like ">" or "*" on the console.
+         */
+        constructor(display_input_function = () => null)
+        {
+            super();
+            vm.assert_type(display_input_function, "function");
+            this.display_input_function = display_input_function;
+            this.buffer = "";
+            this.wake_up_function = null;
+        }
+
+        /*
+         * Set the Lisp function that will be called when new data
+         * arrives.  Called by Lisp.
+         */
+        set_wake_up_function(wake_up_function)
+        {
+            vm.assert_type(wake_up_function, vm.Function);
+            this.wake_up_function = wake_up_function;
+        }
+
+        /*
+         * Add a new line to the input buffer.  Notify Lisp via the
+         * wake-up function.
+         */
+        add_line(line)
+        {
+            vm.assert_type(line, vm.String);
+            this.buffer += line.get_utf8_bytes();
+            if (this.wake_up_function !== null) {
+                /*
+                 * Reset wake-up function to null so we never
+                 * accidentally enter the saved continuation twice.
+                 */
+                const wuf = this.wake_up_function;
+                this.wake_up_function = null;
+                vm.eval_form(vm.list(wuf));
+            } else {
+                console.log("No wakeup function - Lisp crashed?");
+            }
+        }
+
+        /*
+         * Get a fresh input stream that reads the current input
+         * buffer contents.  Called by Lisp.
+         */
+        get_input_stream()
+        {
+            return new vm.String_input_stream(new vm.String(this.buffer));
+        }
+
+        /*
+         * Remove the bytes that have been successfully read via the
+         * input stream from the front of the buffer.  Called by Lisp
+         * once it has read an object.
+         */
+        truncate_input_buffer(input_stream)
+        {
+            vm.assert_type(input_stream, vm.String_input_stream);
+            vm.assert(input_stream.pos > 0);
+            this.buffer = this.buffer.slice(input_stream.pos + 1);
+        }
+    };
+
+    /*
+     * Lisp API
+     */
+
+    vm.define_class("repl:input-buffer", vm.REPL_input_buffer, vm.Object);
+
+    vm.define_alien_function("repl:%%set-input-buffer-wake-up-function",
+                             (buffer, fun) => buffer.set_wake_up_function(fun));
+
+    vm.define_alien_function("repl:%%make-input-buffer-stream",
+                             (buffer) => buffer.get_input_stream());
+
+    vm.define_alien_function("repl:%%truncate-input-buffer",
+                             (buffer, stream) => buffer.truncate_input_buffer(stream));
+
+    vm.define_alien_function("repl:%%display-waiting-for-input-sign",
+                             (buffer) => buffer.display_input_function());
+
+}
