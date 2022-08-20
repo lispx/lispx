c31298ae340eeedbd476c44cc6c6ec573e1ffe3d
Manuel Simoni <msimoni@gmail.com>
2022-08-20 17:40:08 +0200

    repl-wip-3

--- tool/repl/repl.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    const vm = new window["lispx-vm"].VM();
-    const term = $('#terminal').terminal({}, {
-        greetings: vm.write_to_js_string(vm.eval_js_string(`"Welcome to Nybble Lisp!"`))
-    });
-});
