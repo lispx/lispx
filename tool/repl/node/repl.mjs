a170e6c453dc27a89aa26bfebcf35d91ca2620fa
Manuel Simoni <msimoni@gmail.com>
2022-08-27 17:01:04 +0200

    borken-node-repl

--- tool/repl/node/repl.mjs
+++ tool/repl/node/repl.mjs
@@ -1,8 +1,8 @@
 import fs from "fs";
 import readline from "readline";
 import { exit } from "process";
-import { VM } from "kwatz-lisp";
-import { init_repl_stream } from "./repl_stream.js";
+import { VM } from "../../../dist/lispx-vm.min.mjs";
+import { init_repl_stream } from "../repl-stream.mjs";
 
 /*
  * Init the VM and load the REPL code.
@@ -10,14 +10,16 @@ import { init_repl_stream } from "./repl_stream.js";
 
 var vm = new VM();
 init_repl_stream(vm);
-const repl_code = fs.readFileSync("./src/repl.kwatz", "utf-8");
+const repl_code = fs.readFileSync("../repl.lispx", "utf-8");
+const repl_stream_code = fs.readFileSync("../repl-stream.lispx", "utf-8");
 vm.eval_js_string(repl_code);
+vm.eval_js_string(repl_stream_code);
 
 /*
  * Set up Lisp standard output to print to the Node stdout.
  */
 
-vm.STANDARD_OUTPUT.set_value(new vm.REPL_output_stream(function (output) {
+vm.STANDARD_OUTPUT.set_value(new vm.JS_console_output_stream(function (output) {
     process.stdout.write(output.to_js_string());
 }));
 
@@ -46,6 +48,7 @@ rl.on("line", function(line) {
  * Run the REPL.
  */
 
-vm.define_constant("repl:+input-buffer+", input_buffer);
-vm.define_alien_function("repl:%%exit", () => exit(0));
-vm.operate(vm.get_environment().lookup(vm.fsym("repl:run")), vm.nil());
+vm.define_alien_function("repl:%set-debug-level", (level) => {
+});
+
+vm.eval_js_string("(repl:run)");
