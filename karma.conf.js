b746079797cafe5830b3e1abfdb68eea6778ba62
Manuel Simoni <msimoni@gmail.com>
2024-02-04 16:39:50 +0100

    Add TEST_ALL_BROWSERS flag.

--- karma.conf.js
+++ karma.conf.js
@@ -2,8 +2,21 @@
  * Configuration for running browser tests with Karma.
  */
 
+/*
+ * We use Playwright here just because it provides a WebKit browser.
+ */
 const playwright = require("playwright");
 process.env.WEBKIT_HEADLESS_BIN = playwright.webkit.executablePath();
+
+/*
+ * TEST_ALL_BROWSERS is set in the Github CI workflow, causing tests
+ * to be run in all browsers.
+ * For local testing I don't set it, and run tests just in Chrome.
+ */
+const browsers =
+      ("TEST_ALL_BROWSERS" in process.env)
+      ? ["WebkitHeadless", "FirefoxHeadless", "ChromeHeadless"]
+      : ["ChromeHeadless"];
 
 module.exports = function(config)
 {
@@ -14,7 +27,7 @@ module.exports = function(config)
         port: 9876,  // karma web server port
         colors: true,
         logLevel: config.LOG_INFO,
-        browsers: ["WebkitHeadless"],
+        browsers: browsers,
         autoWatch: false,
         singleRun: true, // Karma captures browsers, runs the tests and exits
         concurrency: Infinity
