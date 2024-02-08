78ad0a9c3b242fca99d68fb58e30d51525ad348d
Manuel Simoni <msimoni@gmail.com>
2024-02-08 15:42:17 +0100

    Dev build & test

--- karma.dev.js
+++ karma.dev.js
@@ -1,35 +1,6 @@
 /*
- * Configuration for running browser tests with Karma.
+ * Development-mode configuration for running browser tests with Karma.
  */
+const shared = require("./karma.shared.js");
 
-/*
- * We use Playwright here just because it provides a WebKit browser.
- */
-const playwright = require("playwright");
-process.env.WEBKIT_HEADLESS_BIN = playwright.webkit.executablePath();
-
-/*
- * TEST_ALL_BROWSERS is set in the Github CI workflow, causing tests
- * to be run in all browsers.
- * For local testing I don't set it, and run tests just in Chrome.
- */
-const browsers =
-      ("TEST_ALL_BROWSERS" in process.env)
-      ? ["WebkitHeadless", "FirefoxHeadless", "ChromeHeadless"]
-      : ["ChromeHeadless"];
-
-module.exports = function(config)
-{
-    config.set({
-        frameworks: ["mocha", "chai"],
-        files: ["./dist/lispx-vm-dev.umd.js", "./dist/test/lispx-test-browser.umd.js"],
-        reporters: ["progress"],
-        port: 9876,  // karma web server port
-        colors: true,
-        logLevel: config.LOG_INFO,
-        browsers: browsers,
-        autoWatch: false,
-        singleRun: true, // Karma captures browsers, runs the tests and exits
-        concurrency: Infinity
-    });
-};
+module.exports = (config) => shared("./dist/lispx-vm-dev.umd.js", config);
