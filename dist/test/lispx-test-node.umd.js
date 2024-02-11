68917803be4ae7323e2e2bcd4b2adec4466e3c60
Manuel Simoni <msimoni@gmail.com>
2024-02-11 15:22:48 +0100

    wip 3

--- dist/test/lispx-test-node.umd.js
+++ dist/test/lispx-test-node.umd.js
@@ -15016,8 +15016,10 @@ function init_fasl(vm)
         const [objects, string_map] = vm.collect(target, ext_map);
         const topo_map = vm.topo_sort(objects, ext_map);
         //topo_map.forEach((v, k) => console.log(v + ":" + vm.write_to_js_string(k)));
-        vm.dummy_init(topo_map, ext_map);
-        vm.dummy_fini(topo_map, ext_map);
+        const ctx = { topo_map, ext_map, string_map };
+        vm.dummy_init(ctx);
+        vm.dummy_fini(ctx);
+        console.log("return " + topo_map.get(target));
     };
 
     vm.collect = (target, ext_map) =>
@@ -15090,93 +15092,115 @@ function init_fasl(vm)
         return name + "(" + args.join(",") + ")";
     };
 
-    vm.dummy_ref = (obj, topo_map, ext_map) =>
+    vm.dummy_ref = (obj, ctx) =>
     {
-        if (topo_map.has(obj))
-            return vm.emit_call("toporef", [topo_map.get(obj)]);
-        else if (ext_map.has(obj)) {
-            const ext = ext_map.get(obj);
-            if (ext === "ext-env")
+        if (ctx.topo_map.has(obj)) {
+            return vm.emit_call("toporef", [ctx.topo_map.get(obj)]);
+        } else if (ctx.ext_map.has(obj)) {
+            const name = ctx.ext_map.get(obj);
+            if (name === "ext-env")
                 return "ext_env";
             else
-                // xxx symref
-                return vm.emit_call("extref", [JSON.stringify(vm.write_to_js_string(ext))]);
+                return vm.emit_call("extref", [vm.emit(name, ctx)]);
+        } else {
+            return false;
         }
-        else
-            throw obj;
     };
 
-    vm.Cons.prototype.dummy_init = function(topo_map, ext_map)
+    // INIT
+    vm.Cons.prototype.dummy_init = function(ctx)
     {
         return vm.emit_call("dcons");
     };
-    vm.Function.prototype.dummy_init = function(topo_map, ext_map)
+    vm.Function.prototype.dummy_init = function(ctx)
     {
         return vm.emit_call("dfun");
     };
-    vm.Fexpr.prototype.dummy_init = function(topo_map, ext_map)
+    vm.Fexpr.prototype.dummy_init = function(ctx)
     {
         return vm.emit_call("dfex");
     };
-    vm.Standard_object.prototype.dummy_init = function(topo_map, ext_map)
+    vm.Standard_object.prototype.dummy_init = function(ctx)
     {
-        return vm.emit_call("dobj", [vm.dummy_ref(vm.class_of(this), topo_map, ext_map)]);
+        return vm.emit_call("dobj", [vm.dummy_ref(vm.class_of(this), ctx)]);
     };
-    vm.Standard_class.prototype.dummy_init = function(topo_map, ext_map)
+    vm.Standard_class.prototype.dummy_init = function(ctx)
     {
-        return vm.emit_call("dcls", [vm.dummy_ref(this.get_superclass(), topo_map, ext_map)]);
+        return vm.emit_call("dcls", [vm.dummy_ref(this.get_superclass(), ctx)]);
     };
-    vm.Environment.prototype.dummy_init = function(topo_map, ext_map)
+    vm.Environment.prototype.dummy_init = function(ctx)
     {
         const parent_ref = (this.parent !== null)
-              ? vm.dummy_ref(this.parent, topo_map, ext_map)
+              ? vm.dummy_ref(this.parent, ctx)
               : "null";
         return vm.emit_call("denv", [parent_ref]);
     };
-
-    vm.Cons.prototype.dummy_fini = function(topo_map, ext_map)
+    // FINI
+    vm.Cons.prototype.dummy_fini = function(ctx)
     {
-        return vm.emit_call("fcons", [vm.emit_call("toporef", [topo_map.get(this)]),
-                                      vm.emit(this.car()),
-                                      vm.emit(this.cdr())]);
+        return vm.emit_call("fcons", [vm.dummy_ref(this, ctx), // xxx
+                                      vm.emit(this.car(), ctx),
+                                      vm.emit(this.cdr(), ctx)]);
     };
-    vm.Function.prototype.dummy_fini = function(topo_map, ext_map)
+    vm.Function.prototype.dummy_fini = function(ctx)
     {
-        return vm.emit_call("dfun");
+        return vm.emit_call("ffun", [vm.dummy_ref(this, ctx), vm.emit(this.wrapped_operator, ctx)]);
     };
-    vm.Fexpr.prototype.dummy_fini = function(topo_map, ext_map)
+    vm.Fexpr.prototype.dummy_fini = function(ctx)
     {
-        return vm.emit_call("dfex");
+        return vm.emit_call("ffex", [vm.dummy_ref(this, ctx), "BODY_FORM..."]);
     };
-    vm.Standard_object.prototype.dummy_fini = function(topo_map, ext_map)
+    vm.Standard_object.prototype.dummy_fini = function(ctx)
     {
-        return vm.emit_call("dobj", [vm.dummy_ref(vm.class_of(this), topo_map, ext_map)]);
+        return vm.emit_call("fobj", [vm.dummy_ref(this, ctx), "SLOTS"]);
     };
-    vm.Standard_class.prototype.dummy_fini = function(topo_map, ext_map)
+    vm.Standard_class.prototype.dummy_fini = function(ctx)
     {
-        return vm.emit_call("dcls", [vm.dummy_ref(this.get_superclass(), topo_map, ext_map)]);
+        return vm.emit_call("fcls", [vm.dummy_ref(this, ctx), "METHODS"]);
     };
-    vm.Environment.prototype.dummy_fini = function(topo_map, ext_map)
+    vm.Environment.prototype.dummy_fini = function(ctx)
     {
-        const parent_ref = (this.parent !== null)
-              ? vm.dummy_ref(this.parent, topo_map, ext_map)
-              : "null";
-        return vm.emit_call("denv", [parent_ref]);
+        return vm.emit_call("fenv", [vm.dummy_ref(this, ctx), "BINDINGS"]);
     };
 
-    vm.emit = (obj) => "X";
-
-    vm.dummy_init = (topo_map, ext_map) =>
+    vm.emit = (obj, ctx) =>
     {
-        topo_map.forEach((pos, obj) => {
-            console.log(obj.dummy_init(topo_map, ext_map));
+        const ref = vm.dummy_ref(obj, ctx);
+        if (ref !== false)
+            return ref;
+        else
+            return obj.emit(ctx);
+    };
+
+    vm.Object.prototype.emit = function(ctx) { throw vm.write_to_js_string(this); }
+    vm.Nil.prototype.emit = function(ctx) { return "N"; }
+    vm.Void.prototype.emit = function(ctx) { return "V"; }
+    vm.Ignore.prototype.emit = function(ctx) { return "I"; }
+    vm.Boolean.prototype.emit = function(ctx) { return (this === vm.t()) ? "T" : "F" }
+    vm.Symbol.prototype.emit = function(ctx)
+    {
+        return vm.emit_call(this.namespace, [JSON.stringify(this.get_string().to_js_string())]);
+    };
+    vm.Cons.prototype.emit = function(ctx)
+    {
+        return vm.emit_call("C", [vm.emit(this.car(), ctx), vm.emit(this.cdr(), ctx)]);
+    };
+    vm.Fexpr.prototype.emit = function(ctx)
+    {
+        return vm.emit_call("FEX");
+    };
+
+    vm.dummy_init = (ctx) =>
+    {
+        ctx.topo_map.forEach((pos, obj) => {
+            console.log(obj.dummy_init(ctx));
         });
     };
 
-    vm.dummy_fini = (topo_map, ext_map) =>
+    vm.dummy_fini = (ctx) =>
     {
-        topo_map.forEach((pos, obj) => {
-            console.log(obj.dummy_fini(topo_map, ext_map));
+        ctx.topo_map.forEach((pos, obj) => {
+            console.log(obj.dummy_fini(ctx));
         });
     };
 
